ab3af870415ded4fa5ba842c322b36ce5c004dee
sandyddeveloper <sandyddeveloper@gmail.com>
2024-06-27 10:35:32 +0530

    still

--- chat-app/src/Components/Chats/Chats.jsx
+++ chat-app/src/Components/Chats/Chats.jsx
@@ -1,7 +1,17 @@
 import './chats.css'
 import {avatar, info,phone,videoIcon, emoji, mic, camera,img} from '../../utils/asset'
+import EmojiPicker from "emoji-picker-react";
+import { useState } from 'react';
 
 const Chats = () => {
+  const [open, setOpen] = useState(false);
+  const [text, setText] =useState ("");
+  const handleEmoji = e =>{
+    setText(prev => prev+e.emoji);
+    setOpen(false)
+
+  };
+  
   return (
     <div className='App__chats'>
       <div className="top">
@@ -25,9 +35,20 @@ const Chats = () => {
           <img src={camera} alt="" />
           <img src={mic} alt="" />
           </div>
-          <input type="text" placeholder='Type a message...'/>
+          <input type="text" placeholder='Type a message...' value={text}onChange={(e)=> setText(e.target.value)}/>
+
+            {/* (e) => setText(e.target.value): This is an arrow function that takes the event object (e) as a parameter.
+            
+            e.target.value: This gets the current value of the input field.
+            
+            setText(e.target.value): This updates the text state with the new value from the input field. */}
+
           <div className="emoji">
-            <img src={emoji} alt="" />
+            <img src={emoji} alt="" onClick={() =>setOpen(prev => !prev)}/>
+              <div className="picker">
+            <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
+            
+          </div>
           </div>
           <button className='sendButton'>Send</button> 
         </div>
